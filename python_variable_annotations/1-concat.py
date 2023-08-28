7ec5e5d3e9d73c12ffc4ffa5e74da9af294e6388
vandriodd <ludasaadi@gmail.com>
2023-08-28 10:37:41 -0700

    Basic annotations - concat str

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Basic annotations - concat module
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Concatenate two strings and return the result as a string"""
+    return str1 + str2
