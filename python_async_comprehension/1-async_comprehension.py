5a2586017621092c8dd65b5cb3720101d7e67a9a
vandriodd <ludasaadi@gmail.com>
2023-08-31 12:15:00 -0700

    Add output annotation to coroutine

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -2,9 +2,10 @@
 """
 Async Comprehension - async comprehension module
 """
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     """Coroutine that will collect 10 random numbers using an async for"""
     return [i async for i in async_generator()]
