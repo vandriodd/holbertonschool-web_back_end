2568b0d191a854758f53e152885c8075e941d0a6
vandriodd <ludasaadi@gmail.com>
2023-08-31 11:53:55 -0700

    Add output annotation to coroutine

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -4,9 +4,10 @@ Async Comprehension - async_generator module
 """
 from asyncio import sleep
 from random import uniform
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """Coroutine that loops 10 times, each time asynchronously wait 1 second"""
     for _ in range(10):
         await sleep(1)
