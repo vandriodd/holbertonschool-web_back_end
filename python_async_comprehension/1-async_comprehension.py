7f56f1f8c8c261a611e4092c14017ee44e3632f1
vandriodd <ludasaadi@gmail.com>
2023-08-31 12:12:34 -0700

    Async Comprehensions

---
+++ python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Async Comprehension - async comprehension module
+"""
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension():
+    """Coroutine that will collect 10 random numbers using an async for"""
+    return [i async for i in async_generator()]
