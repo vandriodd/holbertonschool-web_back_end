a0ef5409a6308320bfa292c9ff37b084d6049ad1
vandriodd <ludasaadi@gmail.com>
2023-08-31 08:14:03 -0700

    Create variable to store delay

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -8,5 +8,6 @@ from random import uniform
 
 async def wait_random(max_delay: int = 10) -> float:
     """Asynchronous coroutine that waits with a random delay by given int"""
-    await asyncio.sleep(uniform(0, max_delay))
-    return uniform(0, max_delay)
+    delay = uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
