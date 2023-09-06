b550fe82263333d841e4b5943a354617624e5c4e
vandriodd <ludasaadi@gmail.com>
2023-09-06 12:25:45 -0700

    Delete list cast

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -6,5 +6,5 @@ NoSQL - schools_by_topic module
 
 def schools_by_topic(mongo_collection, topic):
     """Function that returns the list of school having a specific topic"""
-    schools = list(mongo_collection.find({"topic": topic}))
+    schools = mongo_collection.find({"topic": topic})
     return schools
