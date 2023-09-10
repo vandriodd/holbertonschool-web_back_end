fc23f802555dbfa87e83fbb754903ba7ccae839f
vandriodd <ludasaadi@gmail.com>
2023-09-09 18:27:51 -0700

    Exec import

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -5,22 +5,23 @@ NoSQL - log_stats module
 
 from pymongo import MongoClient
 
+if __name__ == "__main__":
+    client = MongoClient("mongodb://localhost:27017/")
+    db = client.logs
+    collection = db.nginx
 
-client = MongoClient("mongodb://localhost:27017/")
-db = client.logs
-collection = db.nginx
+    logs = collection.count_documents({})
+    print(f"{logs} logs")
 
-logs = collection.count_documents({})
-print(f"{logs} logs")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        count_methods = {
+            method: collection.count_documents({"method": method})}
+    print("Methods:")
+    for method, count in count_methods.items():
+        print(f"\tmethod {method}: {count}")
 
-methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-for method in methods:
-    count_methods = {method: collection.count_documents({"method": method})}
-print("Methods:")
-for method, count in count_methods.items():
-    print(f"\tmethod {method}: {count}")
+    status = collection.count_documents({"method": "GET", "path": "/status"})
+    print(f"{status} status check")
 
-status = collection.count_documents({"method": "GET", "path": "/status"})
-print(f"{status} status check")
-
-client.close()
+    client.close()
