1c23c743303815ad5a667e6fa41c81c8023eab46
vandriodd <ludasaadi@gmail.com>
2023-09-15 18:21:34 +0000

    Let's create a report object

---
+++ ES6_basic/12-createReportObject.js
@@ -0,0 +1,9 @@
+export default function createReportObject(employeesList) {
+  const reportObj = {
+    allEmployees: { ...employeesList },
+    getNumberOfDepartments(employeesList) {
+      return Object.keys(employeesList).length;
+    }
+  };
+  return reportObj;
+}
