cedd575dd2f28477d66ce463388ce75505903969
vandriodd <ludasaadi@gmail.com>
2023-09-16 18:01:19 +0000

    Methods, static methods, computed methods names..... MONEY

---
+++ ES6_classes/3-currency.js
@@ -0,0 +1,34 @@
+export default class Currency {
+  constructor(code, name) {
+    this.code = code;
+    this.name = name;
+  }
+
+  validate(propertyValue, property, type = 'string') {
+    /* eslint-disable valid-typeof */
+    if (typeof propertyValue !== type) {
+      throw new TypeError(`${property} must be a ${type}`);
+    }
+    this[`_${property}`] = propertyValue;
+  }
+
+  displayFullCurrency() {
+    return `${this.name} (${this.code})`;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(newCode) {
+    this.validate(newCode, 'code');
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    this.validate(newName, 'name');
+  }
+}
