afae0b5b0dbd2b016a8d0cce743e8f7bc148e7f6
vandriodd <ludasaadi@gmail.com>
2023-09-17 04:45:04 +0000

    EVCar

---
+++ ES6_classes/100-evcar.js
@@ -0,0 +1,21 @@
+import Car from './10-car';
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  static get [Symbol.species]() {
+    return Car;
+  }
+
+  /* eslint-disable comma-dangle */
+  cloneCar() {
+    return new this.constructor[Symbol.species](
+      this.brand,
+      this.motor,
+      this.color
+    );
+  }
+}
