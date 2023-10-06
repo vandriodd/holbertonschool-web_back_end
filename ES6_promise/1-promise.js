65c8869dee42a5303ba075f610a3b4f774cfaee8
vandriodd <ludasaadi@gmail.com>
2023-10-06 12:31:57 -0300

    Don't make a promise...if you know you can't keep it

---
+++ ES6_promise/1-promise.js
@@ -0,0 +1,12 @@
+export default function getResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) {
+      const response = {
+        status: 200,
+        body: 'Success'
+      }
+      resolve(response);
+    }
+    reject(new Error('The fake API is not working currently'));
+  })
+}
