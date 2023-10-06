e1d8145762ff1c6e1edbce3555be982612d6259f
vandriodd <ludasaadi@gmail.com>
2023-10-06 18:34:45 -0300

    All tasks from 4 to 9

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,15 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const singUpPromise = signUpUser(firstName, lastName);
+  const uploadPhotoPromise = uploadPhoto(fileName);
+
+  return Promise.allSettled([singUpPromise, uploadPhotoPromise])
+    .then((results) => results.map((result, index) => (
+      {
+        status: result.status,
+        value: index === 0 ? result.value : result.reason,
+      }
+    )));
+}
