407ce041b4f5a57ec102e020dfcd4ae281a418fc
vandriodd <ludasaadi@gmail.com>
2023-10-13 12:39:37 -0300

    Disable no-param-reassign

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,13 +1,15 @@
 export default function updateStudentGradeByCity(
   arrayStudents,
   city,
-  newGrades
+  newGrades,
 ) {
   return arrayStudents
     .filter((student) => student.location === city)
     .map((student) => {
       const grade = newGrades.filter((grade) => grade.studentId === student.id);
+      /* eslint-disable no-param-reassign */
       student.grade = grade[0] ? grade[0].grade : 'N/A';
       return student;
+      /* eslint-enable */
     });
 }
